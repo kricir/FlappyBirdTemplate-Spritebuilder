4e85ef8f54787d15752e7bc4ede36fbdecf8359e
Richard Kreitel <richard.kreitel@kricir.com>
2015-06-10 11:22:39 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/GamePlayScene.m
+++ FlappyBird.spritebuilder/Source/GamePlayScene.m
@@ -10,11 +10,17 @@
     character = (Character*)[CCBReader load:@"Character"];
     [physicsNode addChild:character];
     [self addObstacle];
+    timeSinceLastObstacle = 0.0f;
 }
 
 -(void)update:(CCTime)delta
 {
     // put update code here
+    timeSinceLastObstacle += delta;
+    if(timeSinceLastObstacle > 2.0f) {
+        [self addObstacle];
+        timeSinceLastObstacle = 0.0f;
+    }
 }
 
 // put new methods here
